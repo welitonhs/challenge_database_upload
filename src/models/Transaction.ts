122698ab5a2bf066210051d5d9d724515f164efd
Weliton Sernajotto <weliton.sernajotto23@gmail.com>
2020-07-10 21:36:15 -0300

    Created migrations and entities about Category and Transaction.

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,40 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column({ enum: ['income', 'outcome'] })
   type: 'income' | 'outcome';
 
+  @Column({ type: 'double precision' })
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn({ name: 'created_at' })
   created_at: Date;
 
+  @UpdateDateColumn({ name: 'updated_at' })
   updated_at: Date;
 }
 
